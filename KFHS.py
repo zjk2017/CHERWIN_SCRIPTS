866d2dfe6b03fdf2d69f15ccafa9928561d18ca2
zjk2017 <2548836866@qq.com>
2024-07-06 10:18:06 +0800

    Update KFHS.py

--- KFHS.py
+++ KFHS.py
@@ -2,7 +2,7 @@
 # -- coding: utf-8 --
 # -------------------------------
 # cron "0 0,8 * * " script-path=xxx.py,tag=匹配cron用
-# const $ = new Env('微信公众号：卡夫亨氏新厨艺')
+# const $ = new Env('微信公众号卡夫亨氏新厨艺')
 
 import os
 import random

210a17581d426f2ca99a31318431a1aa8f7b1682
zjk2017 <2548836866@qq.com>
2024-08-29 18:16:51 +0800

    Update CHERWIN_TOOLS.py

--- CHERWIN_TOOLS.py
+++ CHERWIN_TOOLS.py
@@ -11,7 +11,7 @@ import requests
 from http import HTTPStatus
 from datetime import datetime
 import traceback
-NOW_TOOLS_VERSION = '2024.06.30'
+NOW_TOOLS_VERSION = '2024.08.30'
 if os.path.isfile('DEV_ENV.py'):
     import DEV_ENV
 
